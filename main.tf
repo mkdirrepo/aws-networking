48d51f1675d6067d90b44c229b0dbf41f536c514
ryan-wsl <ryanef39+abstract_packet@gmai.com>
2024-01-11 09:30:23 -0500

    variable updates

--- main.tf
+++ main.tf
@@ -3,7 +3,16 @@ module "networking" {
   version = "1.0.1"
   # insert required variables here
     access_ip = var.access_ip
-    db_subnet_group_name = "wedtest"
-    vpc_tag = "wedtest"
+    db_subnet_group_name = "thurstest"
+    vpc_tag = "thurstest"
+    aws_region = "us-east-1"
+    public_sn_count = 2
+    private_sn_count = 2
+    vpc_cidr = "10.16.0.0/20"
+    public_cidrs = ["10.16.1.0/24"]
+    private_cidrs = ["10.16.2.0/24", "10.16.4.0/24"]
+    rds_cidr_block = "10.16.6.0/24"
+    
+
 }
 
