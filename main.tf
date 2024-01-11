663f83118df52d1c6d98c083b2f52545214d287e
ryan-wsl <ryanef39+abstract_packet@gmai.com>
2024-01-10 23:18:20 -0500

    first

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "networking" {
+  source  = "app.terraform.io/ryanff/networking/aws"
+  version = "1.0.0"
+  # insert required variables here
+    access_ip = var.access_ip
+    db_subnet_group_name = "wedtest"
+    vpc_tag = "wedtest"
+}
+
