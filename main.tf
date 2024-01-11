e47ddc51d0fe83064696367fda0593fdbf9770a4
ryan-wsl <ryanef39+abstract_packet@gmai.com>
2024-01-11 09:07:37 -0500

    rds

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "networking" {
   source  = "app.terraform.io/ryanff/networking/aws"
-  version = "1.0.0"
+  version = "1.0.1"
   # insert required variables here
     access_ip = var.access_ip
     db_subnet_group_name = "wedtest"
