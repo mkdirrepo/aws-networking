482a80dcbb9900d4a24bb0be698a93243f3edaee
ryan-wsl <ryanef39+abstract_packet@gmai.com>
2024-01-11 09:44:59 -0500

    variable updates

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "networking" {
     vpc_cidr = "10.16.0.0/20"
     public_cidrs = ["10.16.1.0/24", "10.16.3.0/24"]
     private_cidrs = ["10.16.2.0/24", "10.16.4.0/24"]
-    rds_cidr_block  = "10.16.4.0/24"
+    rds_cidr_block  = "10.16.0.0/20"
     
 
 }
